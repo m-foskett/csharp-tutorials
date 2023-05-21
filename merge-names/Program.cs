af8c994da6cfe6ac8f30637af4582f8162b9bc30
Mark Foskett <markryanfoskett@gmail.com>
2023-05-22 00:17:46 +1000

    added merge-names problem

---
+++ merge-names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public class MergeNames
+{
+    public static string[] UniqueNames(string[] names1, string[] names2)
+    {
+        // Combine the input arrays together
+        string[] combined = names1.Concat(names2).ToArray();
+        // Remove duplicates
+        combined = combined.Distinct().ToArray();
+
+        return combined;
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+        string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+        Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}
